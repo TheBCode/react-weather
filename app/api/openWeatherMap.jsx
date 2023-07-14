c109043ad2c40c46b1795ef4974798c4f25dc74f
Byron Buyck <bbuyck1@gmail.com>
2023-07-14 13:46:09 -0400

    update url for weather api

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=b508e50d7be6a66ece81ea9edbcb8415&units=imperial'
+const OPEN_WEATHER_MAP_URL = 'https://api.openweathermap.org/data/2.5/weather?appid=b508e50d7be6a66ece81ea9edbcb8415&units=imperial'
 
 
 module.exports = {
